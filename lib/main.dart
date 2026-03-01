b9fb99e3f96b9fd6d117d92a1d9a61c79ba40716
mhdsinas22 <sinasmuhammed2@gmail.com>
2026-03-02 04:00:21 +0530

    isp ready

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   await Supabase.initialize(
-    url: dotenv.env["JIOBASE_URL"]!,
+    url: dotenv.env["SUPABASE_URL"]!,
     anonKey: dotenv.env["SUPABASE_ANON_KEY"]!,
   );
 
